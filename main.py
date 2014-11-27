f2a054579c9707657c12ede4f45bc322e2273149
noah morrison <noah@morrison.ph>
2014-11-26 20:22:51 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+
+import os
+import json
+import shlex
+from subprocess import Popen, PIPE
+from sys import argv
+
+from upload import uploadPost
+
+
+def run_command(cmd, context):
+    script_bin = os.path.expanduser('~/etc/wmup/scripts/')
+    args = shlex.split(cmd)
+    args[0] = script_bin + args[0]
+
+    for i, arg in enumerate(args[1:]):
+        try:
+            args[i + 1] = arg.format(**context)
+        except KeyError:
+            args[i + 1] = ''
+
+    proc = Popen(args)
+    code = proc.wait()
+
+    if code:
+        print('{}: exited with code: {}'.format(args[0], code))
+
+
+def run_commands(cmds, context):
+    for cmd in cmds:
+        if '../' in cmd:
+            raise SyntaxError('"../" is not allowed in commands')
+
+        print('Starting command: {}'.format(cmd))
+        run_command(cmd, context)
+
+
+def parse_item(item):
+    data = {
+        'before': [],
+        'after': [],
+        'upload': True,
+        'publish': True,
+    }
+
+    terms = {}
+
+    for key, value in item.items():
+        if key.startswith('@'):
+            terms[key] = value
+
+        elif key.startswith('!'):
+            pass  # Comment
+
+        elif key is 'before':
+            if type(value) is str:
+                value = [value]
+            data['before'] = value
+
+        elif key is 'after':
+            if type(value) is str:
+                value = [value]
+            data['after'] = value
+
+        else:
+            data[key] = value
+
+    data['path']
+
+    try:
+        data['title']
+    except KeyError:
+        data['title'] = os.path.basename(os.path.splitext(data['path'])[0])
+
+    ctx = dict(data)
+    ctx.update(terms)
+    for key, value in data.items():
+        try:
+            data[key] = value.format(**ctx)
+        except KeyError:
+            data[key] = ''
+
+        except AttributeError:
+            pass
+
+    return (data, terms)
+
+if __name__ == '__main__':
+    config_file = os.path.expanduser('~/etc/wmup/config.json')
+    config = json.load(open(config_file, 'r'))
+
+    for item in config['items']:
+        data, terms = parse_item(item)
+        ctx = dict(data)
+        ctx.update(terms)
+
+        args = dict(data)
+        args['terms'] = {key[1:]: value for key, value in terms.items()}
+
+        run_commands(data['before'], ctx)
+
+        uploadPost(config['auth'], **args)
+
+        run_commands(data['after'], ctx)
---
+++ upload.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.compat import xmlrpc_client
+from wordpress_xmlrpc.methods import posts, taxonomies, media
+
+from os import stat
+from datetime import datetime, timedelta
+
+
+def gen_datetime(date, style=None, zone=0):
+    style = style or '%Y-%m-%d'
+    t = datetime.strptime(date, style)
+    d = timedelta(hours=zone)
+    return t + d
+
+
+def uploadMedia(path, wp):
+    mp3 = open(path, 'rb')
+    data = {'name': path.split('/')[-1],
+            'type': 'audio/mpeg',
+            'bits': xmlrpc_client.Binary(mp3.read())}
+
+    response = wp.call(media.UploadFile(data))
+    url = response['url']
+    size = str(stat(path).st_size)
+    return url+'\n'+size+'\n'+'audio/mpeg'
+
+
+def uploadPost(auth, **args):
+    wp = Client(auth['site'], auth['user'], auth['pass'])
+
+    post = WordPressPost()
+
+    post.post_type = args.get('type', None)
+    post.title = args.get('title', None)
+    post.content = args.get('content', None)
+    post.date = gen_datetime(args['date'], args.get('date-format', None),
+                             args.get('zone', 0))
+
+    post.term_names = args.get('terms', None)
+
+    if args.get('path', None) is not None:
+        post.custom_fields = [{'key': 'enclosure',
+                               'value': uploadMedia(args['path'], wp)
+                               }]
+
+    if args.get('publish', False):
+        post.post_status = 'publish'
+
+    post.id = wp.call(posts.NewPost(post))
